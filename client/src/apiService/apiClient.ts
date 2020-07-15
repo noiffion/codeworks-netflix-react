90c86049ae12916257fb0762723695e49282e1a7
David Kerekes <davkedav@gmail.com>
2020-07-15 07:24:18 +0200

    return types in apiClient corrected

--- client/src/apiService/apiClient.ts
+++ client/src/apiService/apiClient.ts
@@ -1,17 +1,20 @@
+import Movie from '../interfaces/movie-model';
+import Genre from '../interfaces/category-model';
+
 const apiBaseURL = 'https://movied.herokuapp.com';
 
 
-export async function getDiscoverMovies(): Promise<any> {
+export async function getDiscoverMovies(): Promise<Movie[]> {
   return fetch(`${apiBaseURL}/discover`)
     .then(results => results.json());
 }
 
-export async function getCategories(): Promise<any> {
+export async function getCategories(): Promise<Genre[]> {
   return fetch(`${apiBaseURL}/categories`)
     .then(results => results.json());
 }
 
-export async function getMoviesPerCategory(id:number): Promise<any> {
+export async function getMoviesPerCategory(id:number): Promise<Movie[]> {
   return fetch(`${apiBaseURL}/categories/${id}`)
     .then(results => results.json());
 }
